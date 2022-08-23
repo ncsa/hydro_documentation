a19e8ba66b86cabe9d1af2c152191fa5534537f9
Craig P Steffen <craigsteffen@gmail.com>
2022-08-23 10:10:46 -0400

    tweaking config before import
    Setting config so that left column is valid.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NCSA Hydro User Documentation'
+copyright = '2022, University of Illinois'
+author = 'University of Illinois'
 
 release = '0.1'
 version = '0.1.0'
