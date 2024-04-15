60e0512defd5f8c5a9749a64fde0e7bd8028a159
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2024-04-15 13:17:18 -0500

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
   var hub_link = document.createElement("a");
   var hub_text = document.createTextNode("NCSA Documentation Hub");
   hub_link.appendChild(hub_text);
-  hub_link.setAttribute("href", "https://docs.ncsa.illinois.edu");
+  hub_link.setAttribute("href", "https://docs.ncsa.illinois.edu/");
 
   var separator = document.createTextNode(" | ");
 
